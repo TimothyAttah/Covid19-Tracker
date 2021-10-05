4a790c71d5740607b0fe9b27ee71c5ba259d11b3
Timothy Attah <timothyattah733@gmail.com>
2021-10-05 03:51:12 +0100

    Added a fetch api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import "./App.css";
 
 export const App = () => {
+  const [ countries, setCountries ] = useState( [] );
+
+  useEffect( () => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+				.then(res => res.json())
+				.then(data => {
+					const countries = data.map(country => ({
+						name: country.country,
+						value: country.countryInfo.iso2,
+					}));
+					setCountries(countries);
+				});
+    }
+    getCountriesData();
+  },[])
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+		<div className='app'>
+			<div className='app__header'>
+				<h1>covid-19 tracker</h1>
+				<FormControl className='app_dropdown'>
+					<Select variant='outlined' value='abc'>
+						{countries.map(country => (
+							<MenuItem value={country.value}>{country.name}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</div>
+		</div>
+	);
 }
